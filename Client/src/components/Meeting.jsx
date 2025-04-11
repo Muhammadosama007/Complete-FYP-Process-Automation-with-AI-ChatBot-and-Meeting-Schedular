2ccfaeb63530e13714ca0b3357577aed023fd8f9
l1f21bsse0424 <l1f21bsse0424@ucp.edu.pk>
2025-04-11 12:00:19 +0500

    refactor : implement add material option

--- Client/src/components/Meeting.jsx
+++ Client/src/components/Meeting.jsx
@@ -71,7 +71,7 @@ const Meeting = ({ readOnly = false }) => {
                     className="bg-blue-950 text-white px-4 py-2 rounded hover:bg-blue-950 transition"
                     onClick={() => setShowModal(true)}
                 >
-                    Add Meeting
+                    + Add Meeting
                 </button>
             )}
 
